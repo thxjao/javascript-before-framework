a50cb2ee5a515a8b2a397b3b6a5df63946f8b76d
thxjao <jpso.dev@gmail.com>
2021-08-10 10:53:25 -0300

    docs: added module file

---
+++ modules/module.js
@@ -0,0 +1,16 @@
+// Modulos servem para quebrar o código
+//e diferentes arquivos, para facilitar
+// a organização e compartilhamento
+// de código comum
+// o script deve ser adcionado da seguinte maneira
+// <script type="module" src="(diretorio do arquivo)"></script>
+
+// arquivo quadrado.js
+
+export function areaQuadrado(l) {
+  return l * l;
+}
+
+export function perimetroQuadrado(l) {
+  return 4 * l;
+}
